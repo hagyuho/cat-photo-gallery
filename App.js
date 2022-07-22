1d46cfa5c868d2d0300db30639676e7d994167e9
hagyuho <hagyuho90@gmail.com>
2022-07-22 13:01:56 +0900

    finish

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App({ $target }) {
   this.state = {
     nowDirectorys: [],
     beforeDirectorys: [],
-    directoryHistory: ["root"],
+    directoryHistory: [{ name: "root", id: "" }],
   };
 
   this.setState = (nextState) => {
@@ -15,7 +15,11 @@ export default function App({ $target }) {
       ...this.state,
       ...nextState,
     };
-    nodes.setState(this.state.nowDirectorys);
+    nodes.setState({
+      nowDirectorys: this.state.nowDirectorys,
+      directoryHistory: this.state.directoryHistory,
+    });
+    breadcrumb.setState(this.state.directoryHistory);
   };
 
   const breadcrumb = new Breadcrumb({
@@ -23,23 +27,54 @@ export default function App({ $target }) {
     initialState: {
       directoryHistory: this.state.directoryHistory,
     },
+    onClick: async (e) => {
+      const selectedId = e.target.dataset.id;
+      const history = this.state.directoryHistory;
+      console.log("index", history.indexOf(selectedId));
+      if (selectedId !== history[history.length - 1].id) {
+        const items = await fetchDirectoryByNodeId(selectedId);
+        this.setState({
+          nowDirectorys: items,
+          // directoryHistory: [...history.slice(0, history.indexOf(selectedId))],
+        });
+      }
+    },
   });
 
   const nodes = new Nodes({
     $target,
     initialState: {
       nowDirectorys: this.state.nowDirectorys,
+      directoryHistory: this.state.directoryHistory,
     },
     onClick: async (e) => {
-      console.log(e.target.dataset.id);
-      const items = await fetchDirectoryByNodeId(e.target.dataset.id);
-      console.log("fetchDirectoryByNodeId", items);
-      this.setState({ nowDirectorys: items });
+      const { name, id, type } = e.target.closest(".Node").dataset;
+      const history = this.state.directoryHistory;
+
+      if (type === "DIRECTORY") {
+        const items = await fetchDirectoryByNodeId(id);
+        this.setState({
+          nowDirectorys: items,
+          directoryHistory: [...history, { name: name, id: id }],
+        });
+      } else if (type === "PREV") {
+        const prevId = history[history.length - 2].id;
+        const items = prevId
+          ? await fetchDirectoryByNodeId(prevId)
+          : await fetchRoot();
+        this.setState({
+          nowDirectorys: items,
+          directoryHistory: [...history.slice(0, history.length - 1)],
+        });
+      } else if (type === "FILE") {
+        console.log("modal");
+      }
     },
   });
 
   const onLoad = async () => {
     const root = await fetchRoot();
+    console.log(root);
     this.setState({
       nowDirectorys: root,
     });
--- components/Nodes.js
+++ components/Nodes.js
@@ -1,9 +1,15 @@
+const IMAGE_END_POINT =
+  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
+
 export default function Nodes({ $target, initialState, onClick }) {
   this.$element = document.createElement("div");
   this.$element.className = "Nodes";
   $target.appendChild(this.$element);
 
-  this.state = initialState.nowDirectorys;
+  this.state = {
+    nowDirectorys: initialState.nowDirectorys,
+    directoryHistory: initialState.directoryHistory,
+  };
 
   this.setState = (nextState) => {
     this.state = nextState;
@@ -11,24 +17,34 @@ export default function Nodes({ $target, initialState, onClick }) {
   };
 
   this.render = () => {
-    const items = this.state;
-    const image_end_point =
-      "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
+    const { nowDirectorys, directoryHistory } = this.state;
+
     this.$element.innerHTML = `
-        ${items
-          .map(
-            (item) => `
-        <div class= "Node" data-id=${item.id}> 
-         ${
-           item.type === "DIRECTORY"
-             ? '<img src="./assets/directory.png">'
-             : `<img src="${image_end_point}${item.filePath}">`
-         }
-         <div>${item.name}</div>
+          ${
+            directoryHistory.length > 1
+              ? `
+          <div class="Node" data-type="PREV">
+          <img src="./assets/prev.png">
         </div>
         `
-          )
-          .join("")}`;
+              : ""
+          }
+          ${nowDirectorys
+            .map(
+              (item) => `
+          <div class= "Node" data-id=${item.id} data-type=${
+                item.type
+              } data-name=${item.name}> 
+           ${
+             item.type === "DIRECTORY"
+               ? '<img src="./assets/directory.png">'
+               : `<img src="${IMAGE_END_POINT}${item.filePath}">`
+           }
+           <div>${item.name}</div>
+          </div>
+          `
+            )
+            .join("")}`;
   };
 
   this.render();
